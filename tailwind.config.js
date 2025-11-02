4d5d8255b96014cf9c8fa0d6bbfe3a67dafda8e9
Chubinbmt0202 <votrantrunganh@gmail.com>
2025-11-02 13:21:57 +0700

    fix: clean up package.json and package-lock.json, remove unused tailwindcss dependency, and update api.ts to comment out axios configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
         "./index.html",
-        // PHẢI CÓ DÒNG NÀY: Quét tất cả các file .js, .ts, .jsx, .tsx trong thư mục src/
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
